52cef46784595e9286bb7b59592b32d67761d3d0
Rashid Ameer <r.ameer.offical@gmail.com>
2025-10-01 08:15:50 +0500

    refactor(server): rename startApplication to start for brevity
    chore(deps): reorganize package.json dependencies

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import app from "./app.js";
 import { NODE_ENV, PORT } from "./constants/env.js";
 import connectDB from "./config/db.js";
 
-async function startApplication() {
+async function start() {
   await connectDB();
 
   app.listen(PORT, () => {
@@ -11,4 +11,4 @@ async function startApplication() {
   });
 }
 
-startApplication();
+start();
